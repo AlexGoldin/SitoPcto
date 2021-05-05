eae177e123d073bb8eee22712177a1b2ae7e3358
AlexGoldin <alessandrogoldin2003@gmail.com>
2021-05-05 10:19:42 +0200

    a

--- JS/View.js
+++ JS/View.js
@@ -7,11 +7,47 @@ document.getElementById('ButtonViewEdifici').onclick = function() {
     document.getElementById('Strade').style.display = "none"
 }
 
-document.getElementById('ButtonViewEdifici').onclick = function() {
+document.getElementById('ButtonViewMonumenti').onclick = function() {
     document.getElementById('Edifici').style.display = "none"
     document.getElementById('Monumenti').style.display = "block"
     document.getElementById('Piazze').style.display = "none"
     document.getElementById('Reperti').style.display = "none"
     document.getElementById('Statue').style.display = "none"
     document.getElementById('Strade').style.display = "none"
+}
+
+document.getElementById('ButtonViewPiazze').onclick = function() {
+    document.getElementById('Edifici').style.display = "none"
+    document.getElementById('Monumenti').style.display = "none"
+    document.getElementById('Piazze').style.display = "block"
+    document.getElementById('Reperti').style.display = "none"
+    document.getElementById('Statue').style.display = "none"
+    document.getElementById('Strade').style.display = "none"
+}
+
+document.getElementById('ButtonViewReperti').onclick = function() {
+    document.getElementById('Edifici').style.display = "none"
+    document.getElementById('Monumenti').style.display = "none"
+    document.getElementById('Piazze').style.display = "none"
+    document.getElementById('Reperti').style.display = "block"
+    document.getElementById('Statue').style.display = "none"
+    document.getElementById('Strade').style.display = "none"
+}
+
+document.getElementById('ButtonViewStatue').onclick = function() {
+    document.getElementById('Edifici').style.display = "none"
+    document.getElementById('Monumenti').style.display = "none"
+    document.getElementById('Piazze').style.display = "none"
+    document.getElementById('Reperti').style.display = "none"
+    document.getElementById('Statue').style.display = "block"
+    document.getElementById('Strade').style.display = "none"
+}
+
+document.getElementById('ButtonViewStrade').onclick = function() {
+    document.getElementById('Edifici').style.display = "none"
+    document.getElementById('Monumenti').style.display = "none"
+    document.getElementById('Piazze').style.display = "none"
+    document.getElementById('Reperti').style.display = "none"
+    document.getElementById('Statue').style.display = "none"
+    document.getElementById('Strade').style.display = "block"
 }
