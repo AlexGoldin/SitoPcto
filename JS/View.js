53d1dee9033285a15f8ca7f815f7fa3287b5cdec
AlexGoldin <alessandrogoldin2003@gmail.com>
2021-05-05 10:11:27 +0200

    e

--- JS/View.js
+++ JS/View.js
@@ -1,5 +1,5 @@
 document.getElementById('ButtonViewEdifici').onclick = function() {
-    document.getElementById('Edifici').style.display = "content"
+    document.getElementById('Edifici').style.display = "block"
     document.getElementById('Monumenti').style.display = "none"
     document.getElementById('Piazze').style.display = "none"
     document.getElementById('Reperti').style.display = "none"
@@ -9,7 +9,7 @@ document.getElementById('ButtonViewEdifici').onclick = function() {
 
 document.getElementById('ButtonViewEdifici').onclick = function() {
     document.getElementById('Edifici').style.display = "none"
-    document.getElementById('Monumenti').style.display = "content"
+    document.getElementById('Monumenti').style.display = "block"
     document.getElementById('Piazze').style.display = "none"
     document.getElementById('Reperti').style.display = "none"
     document.getElementById('Statue').style.display = "none"
