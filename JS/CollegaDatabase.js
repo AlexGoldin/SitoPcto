9e133980c606e411aabed74be43ee25a472b3749
AlexGoldin <alessandrogoldin2003@gmail.com>
2021-05-20 15:45:03 +0200

    qqq

--- JS/CollegaDatabase.js
+++ JS/CollegaDatabase.js
@@ -1,4 +1,6 @@
 import { Client } from 'pg';
 var connectionString = "postgres://ijhvjdgf:q2LjmBeKdDKD-ovwgkkJexd-cVys6OhX@tai.db.elephantsql.com:5432/ijhvjdgf";
 var pgClient = new Client(connectionString);
-pgClient.connect();
+pgClient.connect().then(() => Console.log("Funziona"));
+
+document.getElementById(prova).innerHTML = pgClient.query("select * from reperti");
