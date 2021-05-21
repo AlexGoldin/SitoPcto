013f6dfafe9f6e8fb56c2d3a5c6adc31ce954f31
AlexGoldin <alessandrogoldin2003@gmail.com>
2021-05-21 18:26:26 +0200

    aaa

--- JS/CollegaDatabase.js
+++
@@ -1,6 +0,0 @@
-import { Client } from 'pg';
-var connectionString = "postgres://tommaso_cattaneo: tommicatt21@postgresql-tommaso.alwaysdata.net:5432/tommaso_cattaneo";
-var pgClient = new Client(connectionString);
-pgClient.connect().then(() => Console.log("Funziona"));
-
-document.getElementById(prova).innerHTML = pgClient.query("select * from reperti");
